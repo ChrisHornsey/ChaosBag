21c9eeb36796f5be8b9a2746d6123fbe8f3a2cee
Chris Hornsey <chris@thehornseys.com>
2024-01-28 10:45:54 +0000

    Implemented regular tokens

--- Token.py
+++ Token.py
@@ -1,7 +1,24 @@
+import SkillTest
+
 class ChaosToken:
-    def __init__(self, modifier, name=""):
+    def __init__(self, modifier, name="", type="Regular"):
         self.mod = modifier
         if(name == ""):
             self.name = str(modifier)
         else:
             self.name = name
+
+        if type == "Regular":
+            self.Resolve = StandardResolution
+
+
+
+def StandardResolution(token, skillValue, dificulty):
+    result = skillValue + token.mod
+
+    if(result < dificulty):
+        outcome = "failed"
+    else:
+        outcome = "succeeded"
+
+    return SkillTest.SkillTestResult(dificulty, result, outcome)
